a57f4d935dbed443349f180bd27b430dd4b0cb55
stevejeh02 <117907750+stevejeh02@users.noreply.github.com>
2022-11-10 11:05:38 +0000

    Update streamlit_app.py
    Fixed quote position

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My Parents New Healhy Diner")
 
-streamlit.header"(Breakfast Menu")
+streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 and Blueberry oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie.")
 streamlit.text("Jard-Boiled Free-Range Egg")
